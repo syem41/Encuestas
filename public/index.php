2a66b785464956024ab19e58144b326aeb6f312e
Ssimeonchavez <slymikesimeonchavez@gmail.com>
2026-08-14 03:45:34 -0500

    Actualización de código desde VS Code

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
